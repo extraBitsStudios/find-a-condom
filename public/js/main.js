80048c1d072c262770c0b55957b18cb7d274bb9b
Jess <jaustin107@gmail.com>
2014-04-23 08:11:44 -0400

    remove zoomControl

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 $( document ).ready( function () {
     $( function () {
-        var map = L.map( 'map' ).setView( [35.5800, -82.5558], 13 );
+        var map = L.map( 'map', {
+            zoomControl: false
+        } ).setView( [35.5800, -82.5558], 13 );
         L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
             maxZoom    : 18
